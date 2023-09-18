731fba05dcf9ac06021304b3f3162f142293ba5f
Xau <frozzy.xfs@gmail.com>
2023-09-18 00:05:24 -0400

    Add error for division by 0

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ class calculator {
     add = (a, b) => a + b;
     subtract = (a, b) => a - b;
     multiply = (a, b) => a * b;
-    divide = (a, b) => a / b;
+    divide = (a, b) => {
+        if (b === 0) throw new Error('Division by 0');
+        return a / b;
+    }
 }
 
 module.exports = { capitalize, reverseString, calculator };
