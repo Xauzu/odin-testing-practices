cb7f352c5d10cc89d2921882a54aafc8d221ba4d
Xau <frozzy.xfs@gmail.com>
2023-09-17 23:43:37 -0400

    Add reverseString function

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ function capitalize(string) {
     return string.slice(0,1).toUpperCase() + string.slice(1);
 }
 
-module.exports = { capitalize };
+function reverseString(string) {
+    return [...string].reverse().join('');
+}
+
+module.exports = { capitalize, reverseString };
